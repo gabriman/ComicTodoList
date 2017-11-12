7faa1e667c70fd04cdf50b84645d0b5eba381221
gabriman <gabrypubli@gmail.com>
2017-11-12 11:41:44 +0100

    First commit

---
+++ src/components/App/index.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import 'normalize-css'
+
+import styles from './app.css'
+
+class App extends Component {
+  constructor(){
+    super();
+  }
+  
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default App
